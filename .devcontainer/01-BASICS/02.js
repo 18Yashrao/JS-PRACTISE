3192ffbbde505db3de5b1194c8b93cda9cf3a4c5
18Yashrao <yashkumarrao677@gmail.com>
2025-02-12 10:42:17 +0000

    datatype

---
+++ .devcontainer/01-BASICS/02.js
@@ -0,0 +1,34 @@
+"use strict";  // treat all  Js code newer version
+// alert (9+9) //  we are use NodeJs not browser
+
+
+console.log(9
+    
+    +9)   // code redability should be high
+
+ console.log(" yash")
+
+
+ let name ="yash"
+ let age ="18"
+ let isloggedIn="true"
+
+
+//number=> 2  to power 53
+//bigint
+//string=>"yash"
+//boolean=> true,false
+//null=> standalone value
+//undefined
+//symbol=> unique
+
+
+
+
+
+
+//object
+
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
